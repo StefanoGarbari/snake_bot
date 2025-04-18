af34402ec05809dd4227c56a0cbb7b397160c0c5
Stefano <stefano.garbari3@gmail.com>
2025-04-18 20:13:32 +0200

    changed snake detection

--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ import cv2
 import pyautogui
 import numpy as np
 import dxcam
+from time import sleep
 
 pyautogui.PAUSE = 0
 BLUE_TO_CELL_RATIO =0.05
@@ -33,6 +34,8 @@ x_grid += 2
 y_grid += 2
 w_grid -= 4
 h_grid -= 4
+
+hsvimg = cv2.cvtColor(img[y_grid : y_grid+h_grid, x_grid  : x_grid+w_grid], cv2.COLOR_RGB2HSV)
 
 #find out grid size
 light_lower = np.array([35, 0, 200])
@@ -79,13 +82,13 @@ def draw_line(img,x,y,dir,color,thickness):
     ye = ys
     xe = xs
 
-    if grid[y][x]=='w':
+    if dir=='w':
         ye -= len(img)//grid_size_y
-    if grid[y][x]=='a':
+    if dir=='a':
         xe -= len(img[0])//grid_size_x
-    if grid[y][x]=='s':
+    if dir=='s':
         ye += len(img)//grid_size_y
-    if grid[y][x]=='d':
+    if dir=='d':
         xe += len(img[0])//grid_size_x
 
     cv2.line(img,(int(xs),int(ys)),(int(xe),int(ye)),color,thickness)
@@ -95,55 +98,110 @@ def cell_to_point(x, y):
     xr = int(len(img[0])*(x+0.5)//grid_size_x)
     return (xr,yr)
 
-def path_to_apple(grid, head, apple):
-    temp = [row[:] for row in grid]
+
+def find_apple(hsvimg):
+    mask = cv2.inRange(hsvimg, red_lower, red_upper)
+    sums = np.array([[np.sum(square(mask, x, y)) for x in range(grid_size_x)] for y in range(grid_size_y)])
+    y, x = np.unravel_index(np.argmax(sums), sums.shape)
+    return (y,x)
+
+def find_path(snake, apple):
+    rtn = [[' ' for col in range(grid_size_x)] for row in range(grid_size_y)]
+    if len(snake) == 0:
+        return rtn
+
+    grid = [[0 for col in range(grid_size_x)] for row in range(grid_size_y)]
+    for i,s in enumerate(snake):
+        grid[s[0]][s[1]] = len(snake) - i
+    
+    grid[snake[0][0]][snake[0][1]] = 0
+    
     to_visit = []
-    to_visit.append(apple)
-    temp[apple[0]][apple[1]] = 'x'
-    temp[head[0]][head[1]] = ' '
+    to_visit.append(snake[0])
+
+    first = True
 
     index = 0
-    while index < len(to_visit): #to_visit[index] != head:
+    while index < len(to_visit) and to_visit[index] != apple:
         y,x = to_visit[index]
-        if x != 0 and temp[y][x-1] == ' ':
-            temp[y][x-1] = 'd'
+        val = grid[y][x] - 1
+        first = False
+        if x != 0 and grid[y][x-1] >= 0 and grid[y][x-1] + val <= 0:
+            grid[y][x-1] = val
             to_visit.append((y,x-1))
-        if y != 0 and temp[y-1][x] == ' ':
-            temp[y-1][x] = 's'
+        if y != 0 and grid[y-1][x] >= 0 and grid[y-1][x] + val <= 0:
+            grid[y-1][x] = val
             to_visit.append((y-1,x))
-        if x != grid_size_x-1 and temp[y][x+1] == ' ':
-            temp[y][x+1] = 'a'
+        if x != grid_size_x-1 and grid[y][x+1] >= 0 and grid[y][x+1] + val <= 0:
+            grid[y][x+1] = val
             to_visit.append((y,x+1))
-        if y != grid_size_y-1 and temp[y+1][x] == ' ':
-            temp[y+1][x] = 'w'
+        if y != grid_size_y-1 and grid[y+1][x] >= 0 and grid[y+1][x] + val <= 0:
+            grid[y+1][x] = val
             to_visit.append((y+1,x))
+        
         index += 1
+
+    grid[snake[0][0]][snake[0][1]] = 0
     
-    y,x = head
-    while (y,x) != apple:
-        grid[y][x] = temp[y][x]
-        if grid[y][x] == 'w':
-            y -= 1
-        elif grid[y][x] == 'a':
-            x -= 1
-        elif grid[y][x] == 's':
-            y += 1
-        elif grid[y][x] == 'd':
-            x += 1
+    if index >= len(to_visit):
+        return rtn
+    
+    y,x = apple
+    while (y,x) != snake[0]:
+        if grid[y][x] == -1:
+            if snake[0][1] == x-1:
+                rtn[y][x-1] = 'd'
+                x -= 1
+            if snake[0][1] == x+1:
+                rtn[y][x+1] = 'a'
+                x += 1
+            if snake[0][0] == y-1:
+                rtn[y-1][x] = 's'
+                y -= 1
+            if snake[0][0] == y+1:
+                rtn[y+1][x] = 'w'
+                y += 1
         else:
-            return
+            if x != 0 and grid[y][x-1] == grid[y][x] + 1:
+                rtn[y][x-1] = 'd'
+                x -= 1
+            elif y != 0 and grid[y-1][x] == grid[y][x] + 1:
+                rtn[y-1][x] = 's'
+                y -= 1
+            elif x != grid_size_x-1 and grid[y][x+1] == grid[y][x] + 1:
+                rtn[y][x+1] = 'a'
+                x += 1
+            elif y != grid_size_y-1 and grid[y+1][x] == grid[y][x] + 1:
+                rtn[y+1][x] = 'w'
+                y += 1
+            else:
+                print("apple: ", apple  )
+                for row in rtn:
+                    for cell in row:
+                        print(cell, end='.')
+                    print()
+                print
+                for row in grid:
+                    for cell in row:
+                        print(cell, end='.')
+                    print()
+                exit()
+            
+    
+    return rtn
+
+
     
 
 
-last_y = -1
-last_x = -1
-head_y = -1
-head_x = -1
+snake = []
+BASE_LENGTH = 4
+score = 0
+apple = find_apple(hsvimg)
+path = find_path(snake, apple)
 
-THRESHOLD = len(img)//grid_size_y * len(img[0])//grid_size_x * 255* BLUE_TO_CELL_RATIO
 
 direction = 'd'
-grid = [['x' if np.sum(square(mask,x,y)) > THRESHOLD else ' ' for x in range(grid_size_x)] for y in range(grid_size_y)]
 
 while True:
     img = camera.get_latest_frame()[y_grid:y_grid+h_grid, x_grid:x_grid+w_grid]
@@ -156,57 +214,49 @@ while True:
         sums = np.array([[np.sum(square(mask, x, y)) for x in range(grid_size_x)] for y in range(grid_size_y)])
         temp_y, temp_x = np.unravel_index(np.argmax(sums), sums.shape)
 
-        if not(temp_y == last_y and temp_x == last_x):
-            last_y = head_y
-            last_x = head_x
-            head_y = temp_y
-            head_x = temp_x
+        if len(snake) == 0 or (temp_y, temp_x) not in snake[:-1]:
+            snake.insert(0, (temp_y, temp_x))
+
+        if snake[0] == apple:
+            score += 1
+            apple = find_apple(hsvimg)
+            path = find_path(snake, apple)
+        
+        if len(snake) > BASE_LENGTH + score:
+            snake.pop()
+        
+        #print(score, len(snake), len(set(snake)), len(snake) == len(set(snake)))
+
+        if path[snake[0][0]][snake[0][1]] == ' ':
+            print("off track!")
+            path = find_path(snake, apple)
+
+        if direction != path[snake[0][0]][snake[0][1]]:
+            direction = path[snake[0][0]][snake[0][1]]
+            pyautogui.press(direction)
         
 
-        draw_rectangle(bgrimg,head_x,head_y,(0,255,255),2)
+        #output
+        draw_rectangle(bgrimg,snake[0][1],snake[0][0],(255,0,0),4)
 
-        mask = cv2.inRange(hsvimg,red_lower, red_upper)
-        sums = np.array([[np.sum(square(mask, x, y)) for x in range(grid_size_x)] for y in range(grid_size_y)])
-        apple_y, apple_x = np.unravel_index(np.argmax(sums), sums.shape)
-        draw_rectangle(bgrimg,apple_x,apple_y,(255,0,0),2)
-        
-        mask = cv2.inRange(hsvimg, blue_lower, blue_upper)
-        #body = cv2.bitwise_or(hsvimg, hsvimg, mask=mask)
-       
+        draw_rectangle(bgrimg,apple[1],apple[0],(50,0,255),2)
+
         for y in range(grid_size_y):
             for x in range(grid_size_x):
-                s = square(mask,x,y)
-                if np.sum(s) > THRESHOLD:
-                    draw_rectangle(bgrimg,x,y,(0,0,255),1)
-
-                if grid[y][x] in ('w','a','s','d'):
-                    draw_line(bgrimg,x,y,grid[y][x],(255,255,255),2)
-                    
-
-        #grid = [['x' if np.sum(square(mask,x,y)) > THRESHOLD else ' ' for x in range(grid_size_x)] for y in range(grid_size_y)]
-
-        #print(grid)
-        #path_to_apple(grid, (head_y, head_x), (apple_y, apple_x))
-        #print(grid)
-        #exit()
-        if grid[head_y][head_x] == ' ' or grid[head_y][head_x] == 'x':
-            print("off track!")
-            print(head_x, head_y)
-            grid = [['x' if np.sum(square(mask,x,y)) > THRESHOLD else ' ' for x in range(grid_size_x)] for y in range(grid_size_y)]
-            path_to_apple(grid, (head_y, head_x), (apple_y, apple_x))
-
-        if direction != grid[head_y][head_x]:
-            direction = grid[head_y][head_x]
-            pyautogui.press(direction)
-
+                if path[y][x] in ('w','a','s','d'):
+                    draw_line(bgrimg,x,y,path[y][x],(0,100,0),2)
         
+        for s in snake:
+            draw_rectangle(bgrimg,s[1],s[0],(255,0,255),1)
+
+
         cv2.imshow("snake", bgrimg)
 
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         cv2.destroyAllWindows()
-        for raw in grid:
-            for cell in raw:
+        for row in path:
+            for cell in row:
                 print(cell, end=' ')
             print()
         exit()
